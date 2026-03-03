4a2e2d7e18c46b07cba9b791ce21ffebe54a5ce9
Matija Zuber <matijazuber273@gmail.com>
2026-03-03 10:25:37 +0100

    Added useState instead of a fixed array, added the onSubmit on a form, and using set made the list display iitems that have been inputed

--- src/components/mainComponent.jsx
+++ src/components/mainComponent.jsx
@@ -1,13 +1,40 @@
+import React from 'react';
+
 function Main() {
+
+  const [ingredients,setIngredients] = React.useState([])
+  
+  const ingredientsListItems = ingredients.map(ingredient => (
+    <li key={ingredient}>{ ingredient}</li>
+  ))
+
+  function handleSubmit(event) {
+
+
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+    
+            setIngredients((prevThings) => [
+            ...prevThings,newIngredient
+          ]);
+
+  }
+
   return (
     <>
         <main>
         <div className="divInput">
-          <form>
+          <form onSubmit={handleSubmit}>
             <input placeholder="e.g oregano" className="input" name="ingredient"></input>
             <button className="buttonInput">+ Add ingredient</button>
-            </form>
+          </form>
+          
         </div>
+        
+        <ul>
+          {ingredientsListItems}
+          </ul>
       </main>
     </>
   )
